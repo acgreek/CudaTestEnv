f02b3045daf40d31178f119aa76620e800fe6780
acgreek <agcarat@gmail.com>
2012-12-29 21:38:07 -0500

    add a test script for mp4

--- test_mp4.sh
+++ test_mp4.sh
@@ -4,12 +4,17 @@ let i=0
 
 while [[ $i -lt 10000 ]]
 do 
-	GenDataMp4 $i
+	./GenDataMp4 $i >& /dev/null
 	result=`./mp4 vecSumA.txt vecSumResult.txt vecSumResult.txt| tail -1`
 	if [[ $result != "All tests passed!" ]]  
 	then 
-		echo failed at $i;
+		echo failed at num $i  result: $result;
 		exit -1
 	fi
-	
+	let x=$i%100
+	if [[ $x -eq 0 ]] 
+	then 
+		echo success upto $i
+	fi
+	let i=$i+1
 done 
